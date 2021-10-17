30c4124973ea47f7061bd1f1a579cb6c356a193b
SRTH <89707882+SantyagoRatkovskih@users.noreply.github.com>
2021-10-17 16:21:44 +1100

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 from kivy.app import App
+from kivy.uix.label import Label
+
+class GameApp(App):
+	def build(self):
+		return Label(text='HELLO WORLD', font_size=70)
+		
+if __name__ == '__main__':
+	GameApp().run()
